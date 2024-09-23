0a07dc3610370532275eefde11ac8fcb63d0dc41
Alexandria <walkerciara24@gmail.com>
2024-09-23 12:48:20 -0400

    Control Flow - Plants

---
+++ control-flow-plants.js
@@ -0,0 +1,33 @@
+//constants - controled items//
+const PI = 3.1415
+const GARDENRadius = 5;
+const PLANTSSpace = 0.8;
+const STARTPLANTSpace = 20;
+
+//PRUNED - MONITORED - PLANTED -- Info in Doc.//
+const area = PI * radius * radius;
+const capacity = math.floor (GARDENRadius/PLANTSSpace)
+
+//Part 1 Growing Pains//
+function calculatePlantGrowth(ByWeeks){
+    return StartPlant * math.pow(2, byweeks);
+}
+function DetermineAction(plantcount) {
+        const percentagecapacity = (plantcount / maxCapacity)* 100;
+        
+        if (percentagecapacity > 80) {
+            return "Pruned";
+        } else if {(percentagecpacity >=50) {
+            return "Monitored";
+        } else {
+            return "Planted";
+        }
+
+        }
+
+        }
+}
+ function
+
+
+//Part 2 - Think Bigger//
